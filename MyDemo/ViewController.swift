cf91ad57ceaad0a3172d54bec3d13376aa3c3df1
李晓冬 <lixiao1k@163.com>
2017-07-14 20:12:09 +0800

    shadow

--- MyDemo/ViewController.swift
+++ MyDemo/ViewController.swift
@@ -13,14 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.red
+        let touchView = UIView(frame: CGRect(x: 60, y: 60, width: 200, height: 200))
+        touchView.backgroundColor = UIColor.black
+        touchView.layer.shadowColor = UIColor.black.cgColor
+        touchView.layer.shadowOffset = CGSize(width: 10.0, height: 10.0)
+        touchView.layer.shadowOpacity = 0.45
+        touchView.layer.shadowRadius = 5.0
+        self.view.addSubview(touchView)
+        
+        let guesture = UITapGestureRecognizer(target: self, action: #selector(self.SingleTap))
+        touchView.addGestureRecognizer(guesture)
+        
+        
+    }
+    
+    func SingleTap(){
+        print("You touched me.")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
