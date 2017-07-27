3a2a4c39913037195a81556734f3dfdc3bfb66eb
李晓冬 <lixiao1k@163.com>
2017-07-27 23:42:18 +0800

    apple web app

--- MyDemo/ViewController.swift
+++ MyDemo/ViewController.swift
@@ -7,43 +7,33 @@
 //
 
 import UIKit
-import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
     
-    var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let bounds = UIScreen.main.bounds
+        let frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+        let webView = UIWebView(frame: frame)
+        webView.delegate = self
+        webView.backgroundColor = UIColor.clear
+        
+        let url = URL(string: "http://www.apple.com/")
+        let urlRequest = NSURLRequest(url: url!)
+        webView.loadRequest(urlRequest as URLRequest)
+        
+        self.view.addSubview(webView)
+        
     }
     
-    @IBAction func touchupInsideCheckBtnAction(sender:UIButton){
-        var message = "当前无网络"
-        let status: NetworkReachabilityManager.NetworkReachabilityStatus = MyNetworkReachabilityManager.sharedManager.networkReachabilityStatus()
-        
-        switch status {
-        case .notReachable:
-            message = "当前无网络"
-            print("当前无网络")
-            break
-        case .reachable(NetworkReachabilityManager.ConnectionType.ethernetOrWiFi):
-            message = "是WIFI网络"
-            print("是WIFI网络")
-            break
-        case .reachable(NetworkReachabilityManager.ConnectionType.wwan):
-            message = "是3G或4G网络"
-            print("是3G或4G网络")
-            break
-        default:
-            message = "当前无网络"
-            break
-        }
-        
-        let alert = UIAlertController(title: "Information", message: message, preferredStyle: UIAlertControllerStyle.alert)
-        let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
-        alert.addAction(OKAction)
-        self.present(alert,animated: true,completion: nil)
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 }
 
